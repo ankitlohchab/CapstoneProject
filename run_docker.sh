bc30d3bc10e6aeb6eb06abe197d52d14b8b22976
ankitlohchab <44613902+ankitlohchab@users.noreply.github.com>
2020-05-01 15:53:24 +0530

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,8 +4,8 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build . -t capstone
+sudo docker build . -t capstone
 
 # Step 2: 
 # List docker images
-docker image ls
+sudo docker image ls
